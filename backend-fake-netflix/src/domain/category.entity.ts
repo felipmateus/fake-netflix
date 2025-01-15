a1731c7760d3ed0a33581e07b64b5219c8a403bb
Felipe Mateus de Carvalho <felipmateus8@gmail.com>
2025-01-15 17:50:23 -0300

    ädjusment

--- backend-fake-netflix/src/domain/category.entity.ts
+++ backend-fake-netflix/src/domain/category.entity.ts
@@ -4,7 +4,7 @@ export type CategoryConstructorProps = {
     name: string;
     description?: string | null;
     is_active?: boolean;
-    created_at?: Date;
+    create_at?: Date;
 }
 
 export type CategoryCreateCommand = {
@@ -18,7 +18,7 @@ export class Category{
     name: string;
     description: string | null;
     is_active: boolean;
-    created_at: Date;
+    create_at: Date;
 
 
     constructor(props: CategoryConstructorProps){
@@ -26,14 +26,14 @@ export class Category{
         this.name = props.name;
         this.description = props.description ?? null;
         this.is_active = props.is_active ?? true;
-        this.created_at = props.created_at ?? new Date();
+        this.create_at = props.create_at ?? new Date();
     }
 
     static create(props: CategoryConstructorProps): Category {
         return new Category(props);
     }
 
-    changeName(name: string): void {
+    changeMame(name: string): void {
         this.name = name;
     }
 
@@ -55,7 +55,7 @@ export class Category{
             name: this.name,
             description: this.description,
             is_active: this.is_active,
-            created_at: this.created_at
+            create_at: this.create_at
         };
     }
 }
